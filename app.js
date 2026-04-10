e34be20fd43c28e98a0a3b2cca35ac356c7e9526
Fernando Champredonde Brandan <fernandochampredonde@gmail.com>
2026-04-10 15:26:20 -0300

    Se agregaron 3 html y un css para su estilo.

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Ruta raíz
+// Servir archivos estáticos (CSS, imágenes, JS) desde /public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas que envían archivos HTML desde la carpeta views
 app.get('/', (req, res) => {
-  res.send('Hola mundo!');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
+app.get('/acerca', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'acerca.html'));
+});
+
+app.get('/contacto', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contacto.html'));
+});
+
 
 // Middleware básico para manejo de errores
 app.use((err, req, res, next) => {
@@ -20,4 +33,4 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
-}); 
+});
