0f93338570c147412a17c3b1650c7727b811425a
Fernando Champredonde Brandan <fernandochampredonde@gmail.com>
2026-04-10 13:37:43 -0300

    Agregé los .html para las rutas / , /acerca y /contacto y modifiqué app.js para los get de estas rutas.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // Middleware
 app.use(express.json());
@@ -7,8 +8,19 @@ app.use(express.urlencoded({ extended: true }));
 
 // Ruta raíz
 app.get('/', (req, res) => {
-  res.send('Hola mundo!');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
+// Página "Acerca"
+app.get('/acerca', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'acerca.html'));
+});
+
+// Página de contacto (GET muestra formulario y datos)
+app.get('/contacto', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contacto.html'));
+});
+
 
 // Middleware básico para manejo de errores
 app.use((err, req, res, next) => {
